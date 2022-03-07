e9fa48e890495e81e4cb295ccca565ab35c7bafb
Deap Daru <deapdaru@gmail.com>
2022-03-07 14:04:35 +0530

    Base url update

--- src/AttendanceTableRange.js
+++ src/AttendanceTableRange.js
@@ -1,46 +1,44 @@
-import React , {useState, useEffect} from "react";
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core/styles";
 import "react-datepicker/dist/react-datepicker.css";
-import Datepicker from './Components/Datepicker';
-import PersistentDrawerLeft from './Components/PersistentDrawerLeft';
+import Datepicker from "./Components/Datepicker";
+import PersistentDrawerLeft from "./Components/PersistentDrawerLeft";
 import CustomizedTable from "./Components/CustomizedTable";
-import Grid from '@material-ui/core/Grid';
-import {useHttp} from './hooks/http';
-const styles = theme => ({ 
-  spacing : {
-    padding:'25px 50px',
+import Grid from "@material-ui/core/Grid";
+import {useHttp} from "./hooks/http";
+const styles = theme => ({
+  spacing: {
+    padding: "25px 50px",
     [theme.breakpoints.down("sm")]: {
       padding: 10,
-      margin: 'auto'
-    }
+      margin: "auto",
+    },
   },
   smallSpacing: {
-    padding:'25px',
+    padding: "25px",
     [theme.breakpoints.down("sm")]: {
       padding: 10,
-      margin: 'auto'
-    }
-  }
-})
- 
+      margin: "auto",
+    },
+  },
+});
+
 const AttendanceTableRange = props => {
-
-  const [startDate,setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [formattedDateStart, setFormattedStartDate] = useState('');
-  const [formattedDateEnd, setFormattedDateEnd] = useState('');
+  const [formattedDateStart, setFormattedStartDate] = useState("");
+  const [formattedDateEnd, setFormattedDateEnd] = useState("");
 
-
-  useEffect(()=>{
-    console.log('use effect is running');
-    var startcompleteDate=startDate;
-    var startdate=startcompleteDate.getDate();
-    var startmonth =startcompleteDate.getMonth()+1;
+  useEffect(() => {
+    console.log("use effect is running");
+    var startcompleteDate = startDate;
+    var startdate = startcompleteDate.getDate();
+    var startmonth = startcompleteDate.getMonth() + 1;
     var startyear = startcompleteDate.getFullYear();
-    var endcompleteDate=endDate;
-    var enddate=endcompleteDate.getDate();
-    var endmonth =endcompleteDate.getMonth()+1;
+    var endcompleteDate = endDate;
+    var enddate = endcompleteDate.getDate();
+    var endmonth = endcompleteDate.getMonth() + 1;
     var endyear = endcompleteDate.getFullYear();
     const formatdDatestart = startdate + "-" + startmonth + "-" + startyear;
     const formatdDateend = enddate + "-" + endmonth + "-" + endyear;
@@ -48,54 +46,66 @@ const AttendanceTableRange = props => {
     setFormattedDateEnd(formatdDateend);
   });
 
-  const [fetchedData] = useHttp(`https://wizdem.pythonanywhere.com/Attendance/get-attendance-of-range/${props.location.state.name}/${props.location.state.div}/${formattedDateStart}/${formattedDateEnd}`,[formattedDateStart,formattedDateEnd])
+  const [fetchedData] = useHttp(
+    `http://unicodeattendance.pythonanywhere.com/Attendance/get-attendance-of-range/${props.location.state.name}/${props.location.state.div}/${formattedDateStart}/${formattedDateEnd}`,
+    [formattedDateStart, formattedDateEnd]
+  );
   const fetchedAttenndance = fetchedData ? fetchedData.attendance : [];
- 
-  const handleChange = (date) => {
-    setStartDate(date);
-  }
-  const handleChangeEnd = (date) => {
-    setEndDate(date);
-  }
 
-  const updateChange = async (event) => {
+  const handleChange = date => {
+    setStartDate(date);
+  };
+  const handleChangeEnd = date => {
+    setEndDate(date);
+  };
+
+  const updateChange = async event => {
     event.preventDefault();
-    var startcompleteDate=startDate;
-    var startdate=startcompleteDate.getDate();
-    var startmonth =startcompleteDate.getMonth()+1;
+    var startcompleteDate = startDate;
+    var startdate = startcompleteDate.getDate();
+    var startmonth = startcompleteDate.getMonth() + 1;
     var startyear = startcompleteDate.getFullYear();
-    var endcompleteDate=endDate;
-    var enddate=endcompleteDate.getDate();
-    var endmonth =endcompleteDate.getMonth()+1;
+    var endcompleteDate = endDate;
+    var enddate = endcompleteDate.getDate();
+    var endmonth = endcompleteDate.getMonth() + 1;
     var endyear = endcompleteDate.getFullYear();
     const formatdDatestart = startdate + "-" + startmonth + "-" + startyear;
     const formatdDateend = enddate + "-" + endmonth + "-" + endyear;
     setFormattedStartDate(formatdDatestart);
     setFormattedDateEnd(formatdDateend);
-  }
-    const { classes } = props;
-    return (
-        <div>
-            <PersistentDrawerLeft />
-              <Grid container>
-                <Grid item  xs={12} sm={12} md={12} lg={8} className={classes.spacing}>
-                  <CustomizedTable attendance={fetchedAttenndance} subject={props.location.state.name}/>
-                </Grid>
-                <Grid item  xs={12} sm={12} md={12} lg={2} className={classes.spacing}>
-                  <Datepicker startDate={startDate} handleChange={handleChange} updateChange={updateChange}/>
-                </Grid>
-                <Grid item  xs={12} sm={12} md={12} lg={2} className={classes.spacing}>
-                < Datepicker startDate={endDate} handleChange={handleChangeEnd} updateChange={updateChange}/>
-                </Grid>
-              </Grid>
-        </div>
-    );
-  }
-
+  };
+  const {classes} = props;
+  return (
+    <div>
+      <PersistentDrawerLeft />
+      <Grid container>
+        <Grid item xs={12} sm={12} md={12} lg={8} className={classes.spacing}>
+          <CustomizedTable
+            attendance={fetchedAttenndance}
+            subject={props.location.state.name}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={2} className={classes.spacing}>
+          <Datepicker
+            startDate={startDate}
+            handleChange={handleChange}
+            updateChange={updateChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={2} className={classes.spacing}>
+          <Datepicker
+            startDate={endDate}
+            handleChange={handleChangeEnd}
+            updateChange={updateChange}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 AttendanceTableRange.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(AttendanceTableRange);
-
--- src/StudentAttend.js
+++ src/StudentAttend.js
@@ -1,17 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { Paper, Grid } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import PersistentDrawerLeft from './Components/PersistentDrawerLeft';
-import {useHttp} from './hooks/http';
-import LinearProgress from '@material-ui/core/LinearProgress';
-
+import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import {Paper, Grid} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import PersistentDrawerLeft from "./Components/PersistentDrawerLeft";
+import {useHttp} from "./hooks/http";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -26,112 +25,129 @@ const CustomTableCell = withStyles(theme => ({
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
-    ['@media (max-width:780px)']: { // eslint-disable-line no-useless-computed-key
-      width: '100%!important'
-    }
+    overflowX: "auto",
+    ["@media (max-width:780px)"]: {
+      // eslint-disable-line no-useless-computed-key
+      width: "100%!important",
+    },
   },
   table: {
     minWidth: 400,
   },
   row: {
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.background.default,
     },
   },
   red: {
-    color:  'red',
+    color: "red",
   },
   green: {
-    color: 'green',
+    color: "green",
   },
 });
 
-
-const StudentAttend = (props)=> {
-
+const StudentAttend = props => {
   let selectedData = null;
 
-  const [fetchedData] = useHttp(`https://wizdem.pythonanywhere.com/Attendance/get-attendance-of-student/${props.location.subject}/${props.location.sapID}`,[]);
-  if(fetchedData){
-    selectedData={
-      attendance:fetchedData.attendance,
-      attendance_count:fetchedData.attendance_count,
-      attendance_total:fetchedData.attendance_total,
-      attendance_percentage:fetchedData.attendance_percentage
-    }
+  const [fetchedData] = useHttp(
+    `http://unicodeattendance.pythonanywhere.com/Attendance/get-attendance-of-student/${props.location.subject}/${props.location.sapID}`,
+    []
+  );
+  if (fetchedData) {
+    selectedData = {
+      attendance: fetchedData.attendance,
+      attendance_count: fetchedData.attendance_count,
+      attendance_total: fetchedData.attendance_total,
+      attendance_percentage: fetchedData.attendance_percentage,
+    };
   }
 
- 
-  
-  
-    const { classes } = props;
-    if(selectedData){
+  const {classes} = props;
+  if (selectedData) {
     return (
       <div>
         <PersistentDrawerLeft />
         <Grid container>
-          <Grid item xs={2}>
-          </Grid>
+          <Grid item xs={2}></Grid>
           <Grid item xs={8}>
-          <Paper style={{padding: 20}}>
-            <Grid container className={classes.table}>
-              <Grid item xs={6}><Typography align='center' variant="h5">Sap ID: {props.location.sapID}</Typography></Grid>
-              <Grid item xs={6} align='center' style={{textAlign: "justify"}}>
-              <Typography variant="h6">Attendance count : {selectedData.attendance_count}</Typography>
-              <Typography variant="h6">Attendance percentage : {selectedData.attendance_percentage}</Typography>
-              <Typography variant="h6">Attendance total : {selectedData.attendance_total}</Typography>
+            <Paper style={{padding: 20}}>
+              <Grid container className={classes.table}>
+                <Grid item xs={6}>
+                  <Typography align="center" variant="h5">
+                    Sap ID: {props.location.sapID}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} align="center" style={{textAlign: "justify"}}>
+                  <Typography variant="h6">
+                    Attendance count : {selectedData.attendance_count}
+                  </Typography>
+                  <Typography variant="h6">
+                    Attendance percentage : {selectedData.attendance_percentage}
+                  </Typography>
+                  <Typography variant="h6">
+                    Attendance total : {selectedData.attendance_total}
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
             </Paper>
-              <Grid item xs={12} sm={12}>
-                <Paper>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <CustomTableCell style={{fontSize:'1rem'}}>Date</CustomTableCell>
-                        <CustomTableCell style={{fontSize:'1rem'}}>Time</CustomTableCell>
+            <Grid item xs={12} sm={12}>
+              <Paper>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <CustomTableCell style={{fontSize: "1rem"}}>
+                        Date
+                      </CustomTableCell>
+                      <CustomTableCell style={{fontSize: "1rem"}}>
+                        Time
+                      </CustomTableCell>
 
-                        <CustomTableCell style={{fontSize:'1rem'}}>Present/Absent</CustomTableCell>
+                      <CustomTableCell style={{fontSize: "1rem"}}>
+                        Present/Absent
+                      </CustomTableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {selectedData.attendance.map(row => (
+                      <TableRow className={classes.row} key={row.id}>
+                        <CustomTableCell component="th" scope="row">
+                          {row.date}
+                        </CustomTableCell>
+                        <CustomTableCell component="th" scope="row">
+                          {row.timing}
+                        </CustomTableCell>
+                        {row.attendance === "1" ? (
+                          <CustomTableCell className={classes.green}>
+                            {row.attendance}
+                          </CustomTableCell>
+                        ) : (
+                          <CustomTableCell className={classes.red}>
+                            {row.attendance}
+                          </CustomTableCell>
+                        )}
                       </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {selectedData.attendance.map(row => (
-                        <TableRow className={classes.row} key={row.id}>
-                          <CustomTableCell component="th" scope="row">
-                            {row.date}
-                          </CustomTableCell>
-                          <CustomTableCell component="th" scope="row">
-                            {row.timing}
-                          </CustomTableCell>
-                          {
-                            row.attendance === "1" ? <CustomTableCell className={classes.green}>{row.attendance}</CustomTableCell> : <CustomTableCell className={classes.red}>{row.attendance}</CustomTableCell>
-                          }
-                          
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </Paper>
-              </Grid>
-            </Grid>
-            <Grid item xs={2}>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
             </Grid>
           </Grid>
+          <Grid item xs={2}></Grid>
+        </Grid>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <PersistentDrawerLeft />
+        <LinearProgress style={{marginTop: "-44px"}} color="secondary" />
       </div>
     );
   }
-  else{
-    return(
-      <div>
-          <PersistentDrawerLeft  />
-          <LinearProgress style={{marginTop: '-44px'}} color="secondary"/>
-        </div>
-    )
-  }
-}
+};
 
 StudentAttend.propTypes = {
   classes: PropTypes.object.isRequired,
